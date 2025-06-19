a3ccc883977babe18777d5e2f67e82966c5addde
Bronwyn Taljaard <bronwyntaljaard@bronwyns-laptop.home>
2025-06-19 15:42:32 +0200

    Added state to set player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const [playerName, setPlayerName] = useState("");
+	const [submitted, setSubmitted] = useState(false);
+
+	const handleChange = (event) => {
+		setSubmitted(false);
+		setPlayerName(event.target.value);
+	};
+
+	const handleClick = () => {
+		setSubmitted(true);
+	};
+
+	return (
+		<section id="player">
+			<h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
+			<p>
+				<input type="text" onChange={handleChange} value={playerName} />
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }
